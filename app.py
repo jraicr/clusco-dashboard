8bd3b84ca7e51c83978e1f7c06e13e792bce3309
J. Rai <jraicr@gmail.com>
2023-05-04 17:33:27 +0100

    Adds throttle to sliders.  Adds channels or modules involved in max and min values.
    - Adds label legend for the selected channel or module.
    
    - Comment some pandas data-frame debug printing

--- app.py
+++ app.py
@@ -16,10 +16,9 @@ gc.enable()
 #gc.set_debug(gc.DEBUG_STATS | gc.DEBUG_LEAK)
 
 hv.extension('bokeh', logo=False)
-pn.extension(loading_spinner='dots', loading_color='#00204e',
-             sizing_mode="stretch_width")
+pn.extension(loading_spinner='dots', loading_color='#00204e', sizing_mode="stretch_width")
 pd.options.plotting.backend = 'holoviews'
-
+pn.config.throttled = True
 
 def restart_server_if_empty_task(interval_sec=60):
     while True:
@@ -58,34 +57,39 @@ def connect_to_database(host, port, database):
     return client[database]
 
 
-def hvplot_df_line(df, x, y, title, dic_opts, color='gray', groupby=None):
-    """_summary_
+def hvplot_df_line(df:pd.DataFrame, x, y, title:str, dic_opts:dict, groupby:str, color:str='green'):
+    """
     Plots a pandas dataframe line plot.
     """
-    if groupby != None:
-        dynamic_map = df.hvplot.line(x=x, y=y, title=title, color=color, groupby=groupby,
-                                     responsive=True, min_height=400, hover_cols=[y, x, groupby])
-    else:
-        dynamic_map = df.hvplot.line(x=x, y=y, title=title, color=color,
-                                     groupby=groupby, responsive=True, min_height=400, hover_cols='all')
+    
+    dynamic_map = df.hvplot.line(x=x, y=y, title=title, color=color, groupby=groupby,
+                                 label='selected ' + groupby,  responsive=True, min_height=400, muted_alpha=0)
 
+    
     options = list(dic_opts.items())
     dynamic_map.opts(**dict(options))
 
     return dynamic_map
 
 
-def hvplot_df_max_min_avg_line(df, x, y, title, dic_opts):
+def hvplot_df_max_min_avg_line(df, x, y, title, dic_opts, category):
     """_summary_
     Plots a pandas dataframe line plot with max, min and average columns in df.
     # TODO: Refactorize this to use only one function for all line plots.
     """
 
-    dynamic_map = df.hvplot.line(x=x, y=['max', 'min', 'avg'], title=title,
-                                 responsive=True, min_height=400, hover_cols=['x', 'y', 'Variable'])
-
+    # dynamic_map = df.hvplot.line(x=x, y=['max', 'min', 'mean'], title=title,
+    #                              responsive=True, min_height=400, hover_cols=[x,y, 'max_' + category, 'min_' + category])
+    max_line = df.hvplot.line(x=x, y='max', title=title, responsive=True, min_height=400, hover_cols=['x', 'y', 'max_' + category], label='max', color='red').opts(show_legend=True, alpha=1, muted_alpha=0)
+        
+    min_line = df.hvplot.line(x=x, y='min', title=title, responsive=True, min_height=400, hover_cols=['x', 'y', 'min_' + category], label='min', color='blue').opts(show_legend=True, alpha=1, muted_alpha=0)
+    
+    mean_line = df.hvplot.line(x=x, y='avg', title=title, responsive=True, min_height=400, hover_cols=['x', 'y'], label='avg', color='black').opts(show_legend=True, alpha=1, muted_alpha=0)
+    
     options = list(dic_opts.items())
-    dynamic_map.opts(**dict(options))
+    
+    dynamic_map = max_line * mean_line * min_line
+    #dynamic_map.opts(**dict(options))
 
     return dynamic_map
 
@@ -95,18 +99,17 @@ def hvplot_df_scatter(df, x, y, title, color, size, marker, dic_opts, cmap="reds
     Plot a scatter graph from a pandas dataframe
     """
     if rasterize == False:
-        plot = df.hvplot.scatter(x=x, y=y, title=title, color=color,
+        plot = df.hvplot.scatter(x=x, y=y, title=title, color=color, label='selected ' + groupby,
                                  size=size, marker=marker, cmap=cmap, groupby=groupby, datashade=datashade, rasterize=rasterize, dynamic=dynamic, responsive=True, min_height=400)
 
     else:
         plot = df.hvplot.scatter(x=x, y=y, title=title, color=color,
-                                 marker=marker, cmap=cmap, groupby=groupby, datashade=datashade, rasterize=rasterize, dynamic=dynamic, responsive=True, min_height=400)
+                                 marker=marker, cmap=cmap, groupby=groupby, datashade=datashade, rasterize=rasterize, dynamic=dynamic, responsive=True, min_height=400, label='mean rasterized values')
 
     options = list(dic_opts.items())
     plot.opts(**dict(options))
 
     return plot
-
 
 def get_data_by_date(collection, property_name, date_time, value_field, id_var, var_name, value_name, remove_temperature_anom=False, search_previous=True):
     """_summary_
@@ -148,7 +151,7 @@ def get_data_by_date(collection, property_name, date_time, value_field, id_var, 
             datetime_values.append(document['date'])
 
     if (len(data_values) > 0):
-        print('Building pandas dataframe...')
+        # print('Building pandas dataframe...')
 
         # Pandas dataframe
         pandas_df = pd.DataFrame(data_values, columns=[
@@ -159,7 +162,7 @@ def get_data_by_date(collection, property_name, date_time, value_field, id_var, 
 
         # Melt dataframe to converts from width df to long,
         # where channel would be a variable and the temperature the value...
-        print('Transforms pandas dataframe from wide to long...')
+        #print('Transforms pandas dataframe from wide to long...')
         pandas_df = pandas_df.melt(
             id_vars=[id_var], var_name=var_name, value_name=value_name)
 
@@ -208,8 +211,59 @@ def empty_plot():
     return empty_plot
 
 
+def build_min_max_avg(df, x, y, category):
+    """_summary_
+    Build a dataframe with the min, max and avg values for each date
+    """
+    def process_chunk(df_chunk):
+        # Group by date and get the max, min and avg values
+        df_agg = df_chunk.groupby(x).agg(
+            max=(y, 'max'),
+            min=(y, 'min'),
+            mean=(y, 'mean'),
+        ).rename(columns={'mean':'avg'}).reset_index()
+
+        # Merge with the original DataFrame to get the category for the max and min values
+        df_agg = df_agg.merge(
+            df_chunk,
+            left_on=[x, 'max'],
+            right_on=[x, y],
+            how='left'
+        ).rename(columns={category: 'max_' + category}).drop(columns=y)
+
+        df_agg = df_agg.merge(
+            df_chunk,
+            left_on=[x, 'min'],
+            right_on=[x, y],
+            how='left'
+        ).rename(columns={category: 'min_' + category}).drop(columns=y)
+
+        # Set the date column as the index
+        df_agg.set_index(x, inplace=True)
+
+        def join_unique_values(series):
+            return ','.join(set(series.astype(str)))
+
+        # Group by date and concatenate unique categories with commas
+        df_agg = df_agg.groupby(x).agg({'max': 'first', 'min': 'first', 'avg': 'first', 'max_' + category: join_unique_values, 'min_' + category: join_unique_values})
+
+        return df_agg
+
+    # Split the data into chunks and process each chunk separately
+    chunksize = 40000
+    result = []
+    for i in range(0, len(df), chunksize):
+        df_chunk = df.iloc[i:i+chunksize]
+        result.append(process_chunk(df_chunk))
+
+    # Concatenate the results
+    df_agg = pd.concat(result)
+
+    return df_agg
+
 def build_dataframe_with_min_max(df, x, y):
-   # Build a pandas dataframe from the original dataframe and select the min and max values for each date
+   # Build a pandas dataframe from the original dataframe and select the min and max values for each datetime value  
+   
     new_df = df.groupby(x)[y].agg(['min', 'max', 'mean'])
     new_df = new_df.reset_index()
     #new_df = new_df.rename(columns={'min': 'min_'+y, 'max': 'max_'+y, 'mean': 'avg_'+y})
@@ -227,16 +281,15 @@ def build_dataframe_with_min_max(df, x, y):
 def plot_data(data, x, y, title, xlabel, ylabel, groupby, cmap_custom, clim):
 
     # Build a pandas dataframe from the original dataframe and select the min and max values for each date corresponding to each group
-    df_with_min_max_avg = build_dataframe_with_min_max(data, x, y)
-
-    print("   - Creating plot for: " + title)
+    # df_with_min_max_avg = build_dataframe_with_min_max(data, x, y)
+    df_with_min_max_avg = build_min_max_avg(data, x, y, groupby)
 
     max_line_plot = hvplot_df_max_min_avg_line(df_with_min_max_avg, x=x, y=y, title=title, dic_opts={
-        'padding': 0.1, 'tools': ['hover'], 'xlabel': xlabel, 'ylabel': ylabel, 'axiswise': True, 'show_legend': True})
+        'padding': 0.1, 'tools': ['hover'], 'xlabel': xlabel, 'ylabel': ylabel, 'axiswise': True, 'show_legend': True}, category=groupby)
 
     # Plot lines grouped by channel from data (channel is selected by widget and just one channel is shown at a time)
     lines_plot = hvplot_df_line(data, x=x, y=y, title=title, dic_opts={
-        'padding': 0.1, 'tools': ['hover'], 'xlabel': xlabel, 'ylabel': ylabel, 'axiswise': True}, groupby=groupby)
+        'padding': 0.1, 'tools': ['hover'], 'xlabel': xlabel, 'ylabel': ylabel, 'axiswise': True}, groupby=groupby, color='purple')
 
     # Plot scatter from data for a single channel
     single_channel_scatter_plot = hvplot_df_scatter(data, x=x, y=y, title=title, color=y, cmap=cmap_custom, size=15, marker='o', dic_opts={
@@ -244,21 +297,25 @@ def plot_data(data, x, y, title, xlabel, ylabel, groupby, cmap_custom, clim):
 
     # Plot scatter from data for all channels (rasterized)
     all_channels_scatter_plot = hvplot_df_scatter(data, x=x, y=y, title=title, color=y, cmap=cmap_custom,  size=20, marker='o', dic_opts={
-                                                  'padding': 0.1, 'tools': [''], 'xlabel': xlabel, 'alpha': 0.25, 'ylabel': ylabel, 'clim': clim}, rasterize=True, dynamic=False)
+                                                  'padding': 0.1, 'tools': [''], 'xlabel': xlabel, 'alpha': 0.15, 'ylabel': ylabel, 'clim': clim}, rasterize=True, dynamic=False)
 
     # Juntamos gráfico de lineas y scatters
-    composite_plot = lines_plot * single_channel_scatter_plot * \
-        all_channels_scatter_plot * max_line_plot
+    composite_plot = lines_plot * single_channel_scatter_plot  * max_line_plot * all_channels_scatter_plot
 
+    # composite_plot = lines_plot * single_channel_scatter_plot * \
+    #     all_channels_scatter_plot
+        
     # Not sure if this have any effects since we are using hvplot_df_max_min_avg_line with this dataframe
     del df_with_min_max_avg, data
     gc.collect()
 
-    return composite_plot.opts(legend_position='top_left', toolbar='above', responsive=True, min_height=400, hooks=[disable_logo])
-
+    return composite_plot.opts(legend_position='top', toolbar='above', responsive=True, min_height=500, hooks=[disable_logo], show_legend=True, legend_opts={"click_policy": "hide"})
 
 def create_plot_panel(initial_data, title, date_picker, property_name, value_field, id_var, var_name, value_name, xlabel, ylabel, remove_temperature_anom, search_previous, cmap, climit, template):
 
+    template.main[0][0][2].object = f'''<h1 style="text-align:center">Making plots...</h1> <h2 style="text-align:center">({title})</h2> '''
+    print("   - Creating plot panel for: " + title)
+    
     @pn.depends(date_picker.param.value, watch=True)
     def thread_update_plot_task(date_picker):
 
@@ -287,7 +344,6 @@ def create_plot_panel(initial_data, title, date_picker, property_name, value_fie
         print('result_panel in getter', result_panel)
         return result_panel
 
-    #@pn.io.with_lock
     def update_plot(date_picker):
         with pn.param.set_values(get_panel_by_property(property_name), loading=True):
 
@@ -323,7 +379,7 @@ def create_plot_panel(initial_data, title, date_picker, property_name, value_fie
                                           var_name: c_widget}, sizing_mode='stretch_width', linked_axes=False)
 
             update_grid(panel=updated_plot_panel)
-            print('[Updating finished]')
+            print(f'[Update Finished] {title}')
 
             # run garbage collector to free memory
             del data, db, clusco_min_collection
@@ -331,7 +387,7 @@ def create_plot_panel(initial_data, title, date_picker, property_name, value_fie
 
             return plot
 
-    #@pn.io.with_lock
+    @pn.io.with_lock
     def update_grid(panel):
 
         match property_name:
@@ -405,28 +461,27 @@ def create_dashboard(template):
 
     # Data retrieved from database
     pacta_temperature_data = get_data_by_date(collection=clusco_min_collection, property_name='scb_pixel_temperature',
-                                              date_time=date_filter, value_field='avg', id_var='date', var_name='channel', value_name='temperature', remove_temperature_anom=True)
+                                              date_time=date_filter, value_field='avg', id_var='date', var_name='channel', value_name='temperature', remove_temperature_anom=False)
 
-    # bug uncaught here
+    # There is a bug very hard to reproduces at this point, it happens sometimes when trying to start the server, but retrying it works...
     # print(pacta_temperature_data)
     # Start date based on the date looked up by pacta_temperature
-    # Nos aseguraremos de que los datos correspondan a la misma fecha, en caso de que se haya realizado la consulta en un dia sin datos y se haya buscado en el dia anterior
     if (len(pacta_temperature_data.index) > 0):
         #start_date = pacta_temperature_data['date'].dt.date[0]
         start_date = pacta_temperature_data.index[0].date()
     else:
         print('No data recovered...')
+        sys.exit()
         # while(len(pacta_temperature_data.index) == 0):
         #     pacta_temperature_data = get_data_by_date(collection=clusco_min_collection, property_name='scb_pixel_temperature',
         #                                               date_time=date_filter, value_field='avg', id_var='date', var_name='channel', value_name='temperature', remove_temperature_anom=True)
 
     # get the first date value from pacta_temperature_data
     # the index dataframe is the date, so we can get the first date value from pacta_temperature_data
-    #start_date = pacta_temperature_data['date'].dt.date[0]
     start_date = pacta_temperature_data.index[0].date()
 
     scb_temperature_data = get_data_by_date(collection=clusco_min_collection, property_name='scb_temperature',
-                                            date_time=start_date, value_field='avg', id_var='date', var_name='module', value_name='temperature', remove_temperature_anom=True)
+                                            date_time=start_date, value_field='avg', id_var='date', var_name='module', value_name='temperature', remove_temperature_anom=False)
 
     scb_humidity_data = get_data_by_date(collection=clusco_min_collection, property_name='scb_humidity',
                                          date_time=start_date, value_field='avg', id_var='date', var_name='module', value_name='humidity', remove_temperature_anom=False)
@@ -475,13 +530,13 @@ def create_dashboard(template):
         (1400/1400, (1, 0, 0))])
 
     pacta_temp_plot_panel = create_plot_panel(pacta_temperature_data, 'PACTA Temperature', date_picker, 'scb_pixel_temperature',
-                                              'avg', 'date', 'channel', 'temperature', 'Time', 'Temperature (ºC)', True, False, cmap_temps, (0, 30), template)
+                                              'avg', 'date', 'channel', 'temperature', 'Time (UTC)', 'Temperature (ºC)', True, False, cmap_temps, (0, 30), template)
     scb_temp_plot_panel = create_plot_panel(scb_temperature_data, 'SCB Temperature', date_picker, 'scb_temperature',
-                                            'avg', 'date', 'module', 'temperature', 'Time', 'Temperature (ºC)', False, False, cmap_temps, (0, 30), template)
+                                            'avg', 'date', 'module', 'temperature', 'Time (UTC)', 'Temperature (ºC)', False, False, cmap_temps, (0, 30), template)
     scb_humidity_plot_panel = create_plot_panel(scb_humidity_data, 'SCB Humidity', date_picker, 'scb_humidity',
-                                                'avg', 'date', 'module', 'humidity', 'Time', 'Humidity (%)', False, False, cmap_humidty, (0, 80), template)
+                                                'avg', 'date', 'module', 'humidity', 'Time (UTC)', 'Humidity (%)', False, False, cmap_humidty, (0, 80), template)
     scb_anode_current_plot_panel = create_plot_panel(scb_anode_current_data, 'Anode Current', date_picker, 'scb_pixel_an_current',
-                                                     'avg', 'date', 'channel', 'anode', 'Time', 'Anode Current (µA)', False, False, cmap_anode, (0, 100), template)
+                                                     'avg', 'date', 'channel', 'anode', 'Time (UTC)', 'Anode Current (µA)', False, False, cmap_anode, (0, 100), template)
     hv_plot_panel = create_plot_panel(hv_data, 'High Voltage', date_picker, 'scb_pixel_hv_monitored',
                                       'avg', 'date', 'channel', 'hv', 'Date', 'HV (V)', False, False, cmap_hv, (10, 1400), template)
 
@@ -566,4 +621,4 @@ def get_user_page():
 
 if __name__ == '__main__':
     pn.serve(get_user_page, port=5006, show=False, static_dirs={'images': './images'}, admin=True, title='Clusco Reports',
-             threaded=True, n_threads=0, check_unused_sessions_milliseconds=5000, unused_session_lifetime=5000, log_level='debug')
+             threaded=True, n_threads=4, check_unused_sessions_milliseconds=5000, unused_session_lifetime=5000, log_level='debug')
