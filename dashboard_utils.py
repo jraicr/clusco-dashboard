c8f2556ea9f75f060ce6cddc6b9992ccd0574dd0
J. Rai <jraicr@gmail.com>
2023-05-27 12:05:30 +0100

    Adds dashboard_utils module comments to generate docs with pdoc

--- dashboard_utils.py
+++ dashboard_utils.py
@@ -1,19 +1,31 @@
 import panel as pn
 
-# function to update the loading message
-def update_loading_message(template, message):
+"""
+Module with utility functions for the dashboard.
+"""
+
+def update_loading_message(template:pn.template.MaterialTemplate, message:str):
+    """
+    Updates and shows a loading message in the dashboard while deploying it for the first time.
+
+    Parameters
+    ----------
+    - `template` (pn.template.MaterialTemplate) The template object from panel.
+    - `message` (str) The message to display in the loading screen.
+    """
     template.main[0][0][2].object = message
     
-# function to display database error
-def display_database_error(template: pn.template.MaterialTemplate, show_alert:bool = False):
+def display_database_error(template: pn.template.MaterialTemplate):
+    """
+    Display a message in the dashboard when the connection to the database fails.
 
-    if not show_alert:
-        template.main[0][0] = pn.Column()
-        template.sidebar[0][0] = pn.Column()
-        db_error_image = pn.pane.PNG('./images/db_error.png', width=100, align='center')
-        db_error_text = pn.pane.Markdown('''<h1 style="text-align:center">Connection to database failed. Check if database is running or contact with a system administrator.</h1>''')
-        main_error_col = pn.Column(pn.layout.VSpacer(), db_error_image,  db_error_text, pn.layout.VSpacer(), sizing_mode='stretch_both')
-        template.main[0][0] = main_error_col
-    else:
-        alert = pn.pane.Alert('## Database not available \nThe connection with the database failed. Check if database is running...', alert_type="warning")
-        template.main[0].insert(0, pn.Column(alert))
+    Parameters
+    ----------
+    - `template` (pn.template.MaterialTemplate) The template object from panel.
+    """
+    template.main[0][0] = pn.Column()
+    template.sidebar[0][0] = pn.Column()
+    db_error_image = pn.pane.PNG('./images/db_error.png', width=100, align='center')
+    db_error_text = pn.pane.Markdown('''<h1 style="text-align:center">Connection to database failed. Check if database is running or contact with a system administrator.</h1>''')
+    main_error_col = pn.Column(pn.layout.VSpacer(), db_error_image,  db_error_text, pn.layout.VSpacer(), sizing_mode='stretch_both')
+    template.main[0][0] = main_error_col
